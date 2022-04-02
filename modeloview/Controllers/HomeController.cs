86e006b892f774d3bd2ff89e3674720b888da691
Honório <hmneton@gmail.com>
2022-04-02 05:09:24 -0300

    update

---
+++ modeloview/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using modeloview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modeloview.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            List<Modelo> modelo = new List<Modelo>();
+
+            modelo.Add(new Modelo { Nome = "123", Valor = 123 });
+            modelo.Add(new Modelo { Nome = "456", Valor = 456 });
+            return View(modelo);
+        }
+
+        public ActionResult Contact()
+        {
+
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ modeloview/Models/Modelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace modeloview.Models
+{
+    public class Modelo
+    {
+        public string Nome { get; set; }
+        public int Valor { get; set; }
+    }
+}
