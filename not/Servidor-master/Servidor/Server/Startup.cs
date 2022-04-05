efd64034120ab734abc50837f47d59b1c48337d8
Honório <hmneton@gmail.com>
2022-04-05 06:39:46 -0300

    update

--- not/Servidor-master/Servidor/Server/Startup.cs
+++ not/Servidor-master/Servidor/Server/Startup.cs
@@ -26,9 +26,29 @@ namespace Servidor.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+              // Replace with your connection string.
+        var connectionString = "server=db;user=xxxx;password=xxxx;database=xxxxx";
+
+        // Replace with your server version and type.
+        // Use 'MariaDbServerVersion' for MariaDB.
+        // Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
+        // For common usages, see pull request #1233.
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
+
+        // Replace 'YourDbContext' with the name of your own DbContext derived class.
+        services.AddDbContext<ApplicationDbContext>(
+            dbContextOptions => dbContextOptions
+                .UseMySql(connectionString, serverVersion)
+                // The following three options help with debugging, but should
+                // be changed or removed for production.
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+        );
+
+            // services.AddDbContext<ApplicationDbContext>(options =>
+            //     options.UseSqlServer(
+            //         Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
