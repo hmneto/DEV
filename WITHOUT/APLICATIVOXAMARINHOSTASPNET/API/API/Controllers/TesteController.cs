52e94db5fc50c6f5d5ac018b7526b3b48fe5df84
Honório <hmneton@gmail.com>
2022-04-13 09:04:54 -0300

    update

---
+++ WITHOUT/APLICATIVOXAMARINHOSTASPNET/API/API/Controllers/TesteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class TesteController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Resposta Servidor";
+        }
+    }
+}
---
+++ WITHOUT/APLICATIVOXAMARINHOSTASPNET/APP/APP/APP/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace APP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ApiRequest(object sender, EventArgs args)
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                try
+                {
+                    var uri = "http://10.0.2.2:29187/teste/Get";
+                    //var uri = "http://192.168.1.1:5102/Teste";
+                    var result = client.GetStringAsync(uri);
+                    Console.WriteLine(result.Result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+        }
+    }
+}
